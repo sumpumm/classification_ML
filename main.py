d756daf6ba8754890be4517b09f75a590dcdf173
sumpumm <samarpan69.piya@gmail.com>
2025-04-30 07:44:02 +0545

    k_means completed

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-from models.unsupervised_learning.k_means import result
+from models.unsupervised_learning.k_means import KMeans
+from sklearn import datasets
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')  # Use Agg backend for saving to file
+import matplotlib.pyplot as plt
 
-print(result())
+
+def main():
+    # Load the dataset
+    X, y = datasets.make_blobs()
+
+    # Cluster the data using K-Means
+    clf = KMeans(k=3)
+    y_pred = clf.predict(X)
+
+    # Visualize the results
+    plt.figure(figsize=(10, 8))
+    plt.scatter(X[:, 0], X[:, 1], c=y_pred, cmap='viridis')
+    plt.title("K-Means Clustering Results")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.savefig('kmeans_plot.png')  # Save plot to file
+    print("Plot has been saved as 'kmeans_plot.png'")
+
+
+if __name__ == "__main__":
+    main()
